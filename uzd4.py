22dbd4aa98679ae99a3efd9398ca0f2c1ff63c59
gustavs52 <gustjaun@madona.edu.lv>
2025-10-27 11:47:15 +0200

    izlaboju uzd4.py
    pirms summas n skaitļa ieliku +

--- uzd4.py
+++ uzd4.py
@@ -4,6 +4,7 @@ summma=0
 skaitlis=1
 
 while skaitlis<=100:
-    summma=skaitlis # summma=summma+skaitlis
-    skaitlis=1
+    summma+=skaitlis # summma=summma+skaitlis
+    skaitlis+=1
+
 print("Summa no 1 līdz 100 ir:summma}")
